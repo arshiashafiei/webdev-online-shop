d45556126848ed9b9ec60f151fe9089439b3c0d1
Arshia Shafiei <arshiashafiei03@gmail.com>
2024-04-14 15:11:11 +0330

    add first navbar

--- electro-shop-nuxt/components/TheNavbar.vue
+++ electro-shop-nuxt/components/TheNavbar.vue
@@ -1,40 +1,39 @@
 <template>
     <div>
-        <b-navbar toggleable="lg" type="dark" variant="info">
-            <b-navbar-brand href="#">NavBar</b-navbar-brand>
+        <b-navbar toggleable="lg" type="light">
+            <b-navbar-brand href="#">
+                <b-img class="logo" alt="Electro" src="/logo.svg" />
+            </b-navbar-brand>
 
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+            <b-container>
+                <b-row align-v="center">
+                    <b-column>
+                        <b-nav-item-dropdown class="border squared-right rounded-left" text="All" right>
+                        <b-dropdown-item href="#">Cat1</b-dropdown-item>
+                        <b-dropdown-item href="#">Cat2</b-dropdown-item>
+                        </b-nav-item-dropdown>
+                    </b-column>
 
-            <b-collapse id="nav-collapse" is-nav>
-            <b-navbar-nav>
-                <b-nav-item href="#">Link</b-nav-item>
-                <b-nav-item href="#" disabled>Disabled</b-nav-item>
-            </b-navbar-nav>
+                    <b-column>
+                        <b-nav-form>
+                            <b-img class="logo" alt="Electro" src="/icons/search-icon.svg" />
+                            <b-form-input type="search" class="squared-left squared-right" placeholder="Search"></b-form-input>
+                            <b-button class="squared-left rounded-right button" type="submit">Search</b-button>
+                        </b-nav-form>
+                    </b-column>
+                </b-row>
+            </b-container>
 
             <!-- Right aligned nav items -->
             <b-navbar-nav class="ml-auto">
-                <b-nav-form>
-                <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-                <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-                </b-nav-form>
-
-                <b-nav-item-dropdown text="Lang" right>
-                <b-dropdown-item href="#">EN</b-dropdown-item>
-                <b-dropdown-item href="#">ES</b-dropdown-item>
-                <b-dropdown-item href="#">RU</b-dropdown-item>
-                <b-dropdown-item href="#">FA</b-dropdown-item>
-                </b-nav-item-dropdown>
-
-                <b-nav-item-dropdown right>
-                <!-- Using 'button-content' slot -->
-                <template #button-content>
-                    <em>User</em>
-                </template>
-                <b-dropdown-item href="#">Profile</b-dropdown-item>
-                <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-                </b-nav-item-dropdown>
+                <div class="d-flex">
+                    <b-img class="icon m5" alt="search" src="/icons/search-icon.svg" />
+                    <b-img class="icon m5" alt="heart" src="/icons/heart-icon.svg" />
+                    <b-img class="icon m5" alt="cart" src="/icons/cart-icon.svg" />
+                    <b-button class="button m5" href="#">Login</b-button>
+                </div>
             </b-navbar-nav>
-            </b-collapse>
         </b-navbar>
     </div>
 </template>
@@ -43,4 +42,53 @@
 </script>
 
 <style scoped>
+    .logo {
+        align-self: stretch;
+        height: 35px;
+        position: relative;
+        max-width: 100%;
+        overflow: hidden;
+        flex-shrink: 0;
+        z-index: 1;
+    }
+
+    .icon {
+        height: 24px;
+        width: 24px;
+        position: relative;
+        overflow: hidden;
+        flex-shrink: 0;
+        min-height: 24px;
+    }
+
+    .squared-left {
+        border-top-left-radius: 0;
+        border-bottom-left-radius: 0;
+    }
+
+    .squared-right {
+        border-top-right-radius: 0;
+        border-bottom-right-radius: 0;
+    }
+
+    .rounded-right {
+        border-top-right-radius: 0.5rem;
+        border-bottom-right-radius: 0.5rem;
+    }
+
+    .rounded-left {
+        border-top-left-radius: 0.5rem;
+        border-bottom-left-radius: 0.5rem;
+    }
+
+    .button {
+        background-color: #1C4E8E;
+        border-color: #1C4E8E;
+        color: white;
+    }
+
+    .button:hover {
+        background-color: darken(#1C4E8E, 10%);
+        border-color: darken(#1C4E8E, 10%);
+    }
 </style>
