c0e2b101a5b5024d6808cb44a2da9c7cfe757753
Arshia Shafiei <arshiashafiei03@gmail.com>
2024-04-15 20:17:25 +0330

    add logo and contact info to footer

---
+++ electro-shop-nuxt/components/TheFooter.vue
@@ -0,0 +1,101 @@
+<template>
+    <b-container fluid>
+        <div class="footer-top">
+            <div class="footer-content">
+                <div class="footer-info">
+                    <div class="contact-info">
+                        <img src="/logo-white.svg" alt="Electro logo" class="logo" />
+                        <div class="phone-number">
+                            <img src="/icons/calling-icon.svg" alt="Phone icon" class="icon" />
+                            <div class="number">(316) 555-0116</div>
+                        </div>
+                        <div class="email">
+                            <img src="/icons/mail-icon.svg" alt="Email icon" class="icon" />
+                            <div class="email-address">electro@example.com</div>
+                        </div>
+                        <div class="address">
+                            <img src="/icons/location-icon.svg" alt="Location icon" class="icon" />
+                            <div class="address-text">
+                            4140 Parker Rd. Allentown, <br />New Mexico 31134
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </b-container>
+</template>
+  
+<style scoped>
+    .footer {
+    display: flex;
+    justify-content: center;
+    }
+
+    .footer-top {
+    background-color: var(--Primary-500, #1c4e8e);
+    width: 100%;
+    padding: 26px 60px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    }
+
+    @media (max-width: 991px) {
+    .footer-top {
+        padding: 0 20px;
+    }
+    }
+
+    .footer-content {
+    max-width: 1140px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    }
+
+    .footer-info {
+    display: flex;
+    justify-content: space-between;
+    gap: 20px;
+    font-size: 16px;
+    color: var(--White-500, #fff);
+    font-weight: 400;
+    line-height: 30px;
+    }
+
+    @media (max-width: 991px) {
+    .footer-info {
+        flex-wrap: wrap;
+    }
+    }
+
+    .contact-info {
+    display: flex;
+    flex-direction: column;
+    }
+
+    .logo {
+    width: 203px;
+    max-width: 100%;
+    aspect-ratio: 5;
+    object-fit: auto;
+    object-position: center;
+    }
+
+    .phone-number,
+    .email,
+    .address {
+    display: flex;
+    gap: 10px;
+    margin-top: 16px;
+    }
+
+    .icon {
+    width: 24px;
+    aspect-ratio: 1;
+    object-fit: auto;
+    object-position: center;
+    margin: auto 0;
+    }
+</style>
