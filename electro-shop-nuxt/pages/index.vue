24c9b9b1e386e09ee732d71d88423f3befa9c575
Arshia Shafiei <arshiashafiei03@gmail.com>
2024-04-21 00:44:32 +0330

    add sales component and more static images

--- electro-shop-nuxt/pages/index.vue
+++ electro-shop-nuxt/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div style="max-width: 1240px;margin-right: auto;margin-left: auto;">
     <Categories/>
-    <!-- <Sales /> -->
+    <Sales />
     <BestSeller/>
     <Advertisement/>
     <TopSeller/>
